be6282d510d989c0bc1cc23093633ae69f4fa0fd
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-09-17 19:51:24 +0200

    afegit comprovador de camps i comprovador de mail

--- angular/practica1/src/app/components/usuari/usuari.component.ts
+++ angular/practica1/src/app/components/usuari/usuari.component.ts
@@ -14,12 +14,24 @@ export class UsuariComponent {
   repeatPassword!: string;
   selectedLanguage!: string;
 
+  isFormValid(): boolean {
+    return !!(this.nom && this.correu && this.password && this.repeatPassword && this.selectedLanguage);
+  }
+
+  isEmailValid(email: string): boolean {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return emailRegex.test(email);
+  }
+
   submit() {
-    if (this.password !== this.repeatPassword) {
+    if (!this.isFormValid()) {
+      console.log("Assegura't d'omplir tots els camps");
+    } else if (!this.isEmailValid(this.correu)) {
+      console.log('El correu no es vàlid');
+    } else if (this.password !== this.repeatPassword) {
       console.log('Les contrassenyes no son iguals');
     } else {
       console.log(this.nom, this.correu ,this.password, this.selectedLanguage);
     }
   }
-
 }
