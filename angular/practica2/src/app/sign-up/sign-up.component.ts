693c82b58b49611535f6717a8bc2f5b7caf9b3e7
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-10-04 20:01:09 +0200

    test sucess

--- angular/practica2/src/app/sign-up/sign-up.component.ts
+++ angular/practica2/src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -13,5 +13,21 @@ signUpForm: any;
     return ['French', 'Spanish', 'Italian', 'German', 'English'].sort();
   } 
 
+  constructor() {
+    this.signUpForm = new FormGroup({
+      user: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      repeatPassword: new FormControl('', Validators.required),
+      languages: new FormControl('', Validators.required)
+    });
+  }
   
+  disabled() {
+    if (this.signUpForm.controls['user'].value && this.signUpForm.controls['email'].value && this.signUpForm.controls['password'].value && this.signUpForm.controls['repeatPassword'].value && this.signUpForm.controls['languages'].value) {
+      return true;
+    } else {
+      return false;
+    }   
+  }  
 }
